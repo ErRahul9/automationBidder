bf917a22bc29844c7943eb01b2a87f48f6a13d76
Rahul Parashar <rahulparashar@Rahuls-MacBook-Pro.local>
2022-06-08 11:16:02 -0700

    added code to connect to redis and get the price based on dimentions

--- automationBidder/connectors.py
+++ automationBidder/connectors.py
@@ -1,7 +1,20 @@
+import json
+
+from rediscluster import RedisCluster
+
+# import rediscluster
 
 
-def connectToCache():
-    print("connecting to Cache")
+def connectToCache(host,port,keyVal):
+    width = 300
+    height = 50
+    startup_nodes = [{"host": host, "port": port}]
+    rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True, skip_full_coverage_check=True)
+    key = rc.hgetall(keyVal)
+    getValue = str(key.get(str(width)+":"+str(height)+"_avg_cpi"))
+    # print(getValue)
+    return getValue
+
 
 def connectTopostgres():
     print("connecting to postgres")
@@ -9,3 +22,11 @@ def connectTopostgres():
 
 def connectToBeeswax():
     print("connecting to postgres")
+
+'''
+w: 300
+      h: 600
+
+'''
+
+print(connectToCache("core-dev-bidder-price.pid24g.clustercfg.usw2.cache.amazonaws.com", 6379,"com.dailymail.online"))
